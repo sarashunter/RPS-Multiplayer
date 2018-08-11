2fbfaafa661b763113ab6634f7d4ce91a04fed72
Sara Hunter <34786124+sarashunter@users.noreply.github.com>
2018-08-11 15:17:37 -0400

    Fix chat user issue.

--- assets/logic.js
+++ assets/logic.js
@@ -1,68 +1,103 @@
-// Initialize Firebase
-var config = {
+  // Initialize Firebase
+  var config = {
     apiKey: "AIzaSyCuzvUqY6J85bI9k7wk7ZJ-e-TFIWuT2Uo",
     authDomain: "rpschat-644c9.firebaseapp.com",
     databaseURL: "https://rpschat-644c9.firebaseio.com",
     projectId: "rpschat-644c9",
-    storageBucket: "",
+    storageBucket: "rpschat-644c9.appspot.com",
     messagingSenderId: "21498261849"
-};
-firebase.initializeApp(config);
+  };
+  firebase.initializeApp(config);
 
 var database = firebase.database();
 
+//This could potentially hold more than one chat in the future.
 var chatsRef = database.ref("/chats");
-var mainChatRef = database.ref("/chats/mainChat")
-console.log(mainChatRef);
-var usersRef = database.ref("/users");
-var usersOnlineRef = database.ref("/usersOnline/")
+
+//Only chat for now.
+var mainChatRef = database.ref("/chats/mainChat");
+
+//Holds the user information for current connections.  Users get deleted when they get disconnected.
+var usersOnlineRef = database.ref("/usersOnline/");
+
+//Built in Firebase feature that detects user connections.  Used to tell when users are on.
 var connectedRef = database.ref(".info/connected");
-var currentUserKey;
+
+//This will store the key to the user so other information about the user can be accessed later.
+var currentUserKey;  
+
+//This is the current username.  This is for ease of use and gets valued when a username is chosen.
 var currentUserName;
 
-// When the client's connection state changes...
+// This checks when the number of connections changes.
 connectedRef.on("value", function (snapshot) {
 
-    // If they are connected..
+    // When they are connected
     if (snapshot.val()) {
 
-        // Add user to the connections list.
-       var connected = usersOnlineRef.push({name: "unknown",
-       location: "usa"});
-       currentUserKey = connected.key;
-       console.log(currentUserKey);
-        // Remove user from the connection list when they disconnect.
+        // User gets added to online users
+        var connected = usersOnlineRef.push({
+            name: "unknown", //starts as unknown
+            location: "usa"  //placeholder for later features
+        });
+
+        // Store the "key" to the current user
+        currentUserKey = connected.key;
+
+        // Remove user and their data from the onlineUsers when disconnected
+
         connected.onDisconnect().remove();
     }
 });
 
-
+//function that checks for new messages and runs when the page is loaded
 mainChatRef.on("child_added", function (snapshot) {
+
+    //create a div to show the message
     var $messageDiv = $("<div>").text(snapshot.val().sender + ": " + snapshot.val().message);
+
+    //Append the single message to the chat log
     $("#chatlog").append($messageDiv);
-    console.log(snapshot.val().message);
+
 })
 
+//Function called when post message button is cliced.
 $("#postMessage").on("click", function (event) {
+
     event.preventDefault();
+
+    //Calls function that creates the message
     addMessage($("#messageText").val());
-    $("#messageText").text("");
+
+    //Clears input field for next message.
+    $("#messageText").val("");
 
 })
+
 //function to add new chat message
 function addMessage(messageString) {
+
     mainChatRef.push({
-        sender:  currentUserName,
+        sender: currentUserName,
         message: messageString,
         time: "now"
     })
+
 }
 
 //function to choose your username
 $("#userChoice").on("click", function (event) {
     event.preventDefault();
 
-    currentUserName=$("#chooseUser").val();
+    //Store the value of the username chosen by user.  Probably want to validate this against other users.
+    currentUserName = $("#chooseUser").val(); 
+
+    //Set the name of the current user in user object to user's input.
     database.ref("usersOnline/" + currentUserKey + "/name").set(currentUserName);
+
+    //Remove username input box after username is chosen
+    $("#enterUser").empty();
+
+    //Probably want to add our chat box at this point rather than from the start.
 
 })
